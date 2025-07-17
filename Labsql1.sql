f30a66c934c0d9f16dafb72dc3612e5d739c478c
amonkung <amonkikuio@gmail.com>
2025-07-17 15:42:24 +0700

    Add files via upload

---
+++ Labsql1.sql
@@ -0,0 +1,6 @@
+SELECT * from Employees
+SELECT EmployeeID, FirstName, LastName FROM Employees
+SELECT * FROM Employees WHERE City = 'London'
+SELECT EmployeeID, FirstName, LastName FROM Employees WHERE City = 'London'
+SELECT City, Country FROM Customers
+SELECT Distinct City, Country FROM Customers
