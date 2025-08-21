0195bfea08aa4cd326c62517267e2d00dd55df0f
amonkung <amonkikuio@gmail.com>
2025-08-21 14:20:58 +0700

    Update lab-21-8.sql

--- lab-21-8.sql
+++ lab-21-8.sql
@@ -21,3 +21,19 @@ select e.EmployeeID,FirstName, o.OrderID
 from Employees e join Orders o on e.EmployeeID = o.EmployeeID
 order by EmployeeID
 
+SELECT O.OrderID , C.CompanyName ,
+E.FirstName , O.ShipAddress 
+FROM Orders O, Customers C, Employees E
+WHERE O.CustomerID=C.CustomerID
+AND O.EmployeeID=E.EmployeeID
+
+SELECT O.OrderID , C.CompanyName ,
+E.FirstName , O.ShipAddress 
+FROM Orders O
+join Customers C on O.CustomerID=C.CustomerID
+join Employees E on O.EmployeeID=E.EmployeeID
+
+select e.EmployeeID,FirstName, count(*) as [order]
+from Employees e join Orders o on e.EmployeeID = o.EmployeeID
+where year(OrderDate) = 1998
+group by e.EmployeeID, FirstName
