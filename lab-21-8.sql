d0435bf5ae6b738abfe7cf49f1422eca708ef5de
amonkung <amonkikuio@gmail.com>
2025-08-21 14:11:14 +0700

    Update lab-21-8.sql

--- lab-21-8.sql
+++ lab-21-8.sql
@@ -9,35 +9,15 @@ from Products as p join Categories  as c
 on p.CategoryID=c.CategoryID
 where CategoryName = 'seafood'
 
-select CompanyName,OrderID
-from Orders, Shippers
-where Shippers.ShipperID = Orders.ShipVia
+select* from Orders where OrderID = 10250
+select* from [Order Details] where OrderID = 10250
 
-select CompanyName,OrderID
-from Orders join Shippers
-on Shippers.ShipperID = Orders.ShipVia
+select p.ProductID,p.ProductName,s.CompanyName,s.Country
+from Products p join Suppliers s
+on p.SupplierID = s.SupplierID
+where Country in ('usa', 'uk')
 
-SELECT O.OrderID , C.CompanyName ,
-E.FirstName , O.ShipAddress 
-FROM Orders O, Customers C, Employees E
-WHERE O.CustomerID=C.CustomerID
-AND O.EmployeeID=E.EmployeeID
-
-SELECT O.OrderID , C.CompanyName ,
-E.FirstName , O.ShipAddress
-FROM Orders O join Customers C on O.CustomerID=C.CustomerID
-join Employees E on O.EmployeeID=E.EmployeeID
-
-select e.EmployeeID, FirstName , count(*) as [order]
-, sum(freight) as [Sum of Freight]
+select e.EmployeeID,FirstName, o.OrderID
 from Employees e join Orders o on e.EmployeeID = o.EmployeeID
-where year(orderdate) = 1998
-group by e.EmployeeID, FirstName
+order by EmployeeID
 
-Select Title From Employees
-Where FirstName = 'Nancy'
-
-Select *
-From Employees
-Where Title = (Select Title From Employees
-Where FirstName = 'Nancy')
